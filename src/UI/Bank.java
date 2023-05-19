6d2ca7c64f75799da0ba5a3f259995bb218930bb
Obot-01 <132311109+Obot-01@users.noreply.github.com>
2023-05-19 21:07:18 +0100

    Update Bank.java

--- src/UI/Bank.java
+++ src/UI/Bank.java
@@ -1,1 +1,15 @@
+package UI;
+
+public class Bank {
+
+    float userBal(float num2){
+        int num1 = 100000;
+        return num1 + num2;
+    }
+
+    float userWith(float num2){
+        int num1 = 100000;
+        return num1 - num2;
+    }
+}
 
