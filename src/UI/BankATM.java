87ad35a127fbb61c22838df7ff86192c610db095
UCBinghamHub <132622087+UCBinghamHub@users.noreply.github.com>
2023-05-26 12:18:06 +0100

    Update BankATM.java

--- src/UI/BankATM.java
+++ src/UI/BankATM.java
@@ -1,1 +1,12 @@
+package UI;
+
+public class BankATM extends Bank {
+//    char uniChar = '\u20A6';
+    float balance1(float num2) {
+        return userBal(num2);
+    }
+    float withdrawalBalance(float num2) {
+        return userWith(num2);
+    }
+}
 
